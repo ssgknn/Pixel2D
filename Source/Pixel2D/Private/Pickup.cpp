c7d1f1a1c0a9c2000f2c9f42abb7ae56eb6117c3
ssgknn <ssgknn@gmail.com>
2024-02-12 22:16:47 +0100

    Pickup functionality

--- Source/Pixel2D/Private/Pickup.cpp
+++ Source/Pixel2D/Private/Pickup.cpp
@@ -2,12 +2,32 @@
 
 
 #include "Pickup.h"
+#include "Net/UnrealNetwork.h"
+#include "Engine/ActorChannel.h"
+
+#include "Item.h"
+#include "InventoryComponent.h"
+#include "ZDPlayerCharacterBase.h"
+#include "Pixel2DPlayerController.h"
 
 // Sets default values
 APickup::APickup()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+void APickup::InitializePickup(const TSubclassOf<class UItem> ItemClass, const int32 Quantity)
+{
+	if (HasAuthority() && ItemClass && Quantity)
+	{
+		Item = NewObject<UItem>(this, ItemClass);
+		Item->SetQuantity(Quantity);
+
+		OnRep_Item();
+
+		Item->MarkDirtyForReplication();
+	}
 
 }
 
@@ -16,12 +36,119 @@ void APickup::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	if (HasAuthority() && ItemTemplate && bNetStartup)
+	{
+		InitializePickup(ItemTemplate->GetClass(), ItemTemplate->GetQuantity());
+	}
+
+	/**If pickup was spawned in at runtime, ensure that it matches the rotation of the ground that it was dropped on
+	If we dropped a pickup on a 20 degree slope, the pickup would also be spawned at a 20 degree angle*/
+	if (!bNetStartup)
+	{
+		AlignWithGround();
+	}
+
+	if (Item)
+	{
+		Item->MarkDirtyForReplication();
+	}
+
 }
 
-// Called every frame
-void APickup::Tick(float DeltaTime)
+void APickup::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
 {
-	Super::Tick(DeltaTime);
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	
+	DOREPLIFETIME(APickup, Item);
 
 }
 
+void APickup::OnRep_Item()
+{
+	if (Item)
+	{
+		PickupMesh->SetStaticMesh(Item->PickupMesh);
+		//InteractionComponent->InteractableNameText = Item->DisplayName;
+
+		//Clients bind to this delegate in order to refresh the interaction widget if item quantity changes
+		Item->OnItemModified.AddDynamic(this, &APickup::OnItemModified);
+	}
+
+
+	//If any replicated properties on the item are changed, we refresh the widget
+	//InteractionComponent->RefreshWidget();
+}
+
+void APickup::OnItemModified()
+{
+	/*if (InteractionComponent)
+	{
+		InteractionComponent->RefreshWidget();
+	}*/
+}
+
+bool APickup::ReplicateSubobjects(class UActorChannel* Channel, class FOutBunch* Bunch, FReplicationFlags* RepFlags)
+{
+	bool bWroteSomething = Super::ReplicateSubobjects(Channel, Bunch, RepFlags);
+
+	if (Item && Channel->KeyNeedsToReplicate(Item->GetUniqueID(), Item->RepKey))
+	{
+		bWroteSomething |= Channel->ReplicateSubobject(Item, *Bunch, *RepFlags);
+	}
+
+	return bWroteSomething;
+}
+
+#if WITH_EDITOR
+void APickup::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
+{
+	Super::PostEditChangeProperty(PropertyChangedEvent);
+
+	FName PropertyName = (PropertyChangedEvent.Property != NULL) ? PropertyChangedEvent.Property->GetFName() : NAME_None;
+
+	//If a new pickup is selected in the property editor, change the mesh to reflect the new item being selected
+	if (PropertyName == GET_MEMBER_NAME_CHECKED(APickup, ItemTemplate))
+	{
+		if (ItemTemplate)
+		{
+			PickupMesh->SetStaticMesh(ItemTemplate->PickupMesh);
+		}
+	}
+}
+#endif
+
+void APickup::OnTakePickup(class AZDPlayerCharacterBase* Taker)
+{
+	if (!Taker)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Pickup was taken but player was not valid. "));
+		return;
+	}
+
+	//Not 100% sure Pending kill check is needed but should prevent player from taking a pickup another player has already tried taking
+	if (HasAuthority() && !IsPendingKillPending() && Item)
+	{
+		if (UInventoryComponent* PlayerInventory = Taker->PlayerInventory)
+		{
+			const FItemAddResult AddResult = PlayerInventory->TryAddItem(Item);
+
+			if (AddResult.AmountGiven < Item->GetQuantity())
+			{
+				Item->SetQuantity(Item->GetQuantity() - AddResult.AmountGiven);
+			}
+			else if (AddResult.AmountGiven >= Item->GetQuantity())
+			{
+				Destroy();
+			}
+
+			if (!AddResult.ErrorText.IsEmpty())
+			{
+				if (APixel2DPlayerController* PC = Cast<APixel2DPlayerController>(Taker->GetController()))
+				{
+					// need to move controller logic to player controller
+					//PC->ClientShowNotification(AddResult.ErrorText);
+				}
+			}
+		}
+	}
+}
