fcf39ee01e2849e85288d3d8c29089db710d3a72
ssgknn <ssgknn@gmail.com>
2024-03-04 01:18:26 +0100

    add EquippableItem +add GearEquippableItem

---
+++ Source/Pixel2D/Public/EquippableItem.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Item.h"
+#include "EquippableItem.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Abstract, NotBlueprintable)
+class PIXEL2D_API UEquippableItem : public UItem
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/Pixel2D/Public/GearEquippableItem.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EquippableItem.h"
+#include "GearEquippableItem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PIXEL2D_API UGearEquippableItem : public UEquippableItem
+{
+	GENERATED_BODY()
+	
+};
